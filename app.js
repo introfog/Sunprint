96356a568f9414fd77fb08a45dc1563126c30472
introfog <chubrickdm@gmail.com>
2018-04-08 16:48:06 +0300

    Work with server was added.

--- app.js
+++ app.js
@@ -1,69 +1,146 @@
-const http = require("http");
-const fs = require("fs");
-const path = require('path');
- 
-const server = http.createServer(function(request, response){
-   	//console.log('------request ' + request.url);
+const express = require('express');
+const fs=require('fs');
+const app = express();
+const parser = require('body-parser');
 
-   	let filePath;
-   	if (request.url === "/"){
-		filePath = './public/UI/index.html';
-   	}
-   	else if (request.url.startsWith ('/Photos')){
-   		filePath = "./public/UI" + request.url;
-   	}
-   	else{
-   		filePath = "./public/UI/" + request.url;
-   	}
+app.use(express.static('public'));
+app.use(parser.json());
 
-    let extname = path.extname(filePath);
-    let contentType;
-    switch (extname) {
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.json':
-            contentType = 'application/json';
-            break;
-        case '.png':
-            contentType = 'image/png';
-            break;
-        case '.jpg':
-            contentType = 'image/jpg';
-            break;
-        case '.wav':
-            contentType = 'audio/wav';
-            break;
-        default:
-        	contentType = 'text/html';
-        break;
-    }
-
-    fs.readFile(filePath, function(error, content) {
-        if (error) {
-        	console.log ("--------\nError = " + error);
-        	console.log ("Filepath = " + filePath);
-        	console.log ("Contenr type = " + contentType);
-            if(error.code == 'ENOENT'){
-                response.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
-            }
-            else {
-                response.writeHead(500);
-                response.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
-                response.end();
-            }
-        }
-        else {
-            response.writeHead(200, { 'Content-Type': contentType });
-            response.end(content, 'utf-8');
-        }
-    });
-
+app.listen(3000, ()=> {
+    console.log('Server running on port 3000');
 });
 
-server.listen (3000);
 
-console.log('Server running');
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get('/getPhotoPost', (req, res)=>{
+    let photoPosts = JSON.parse (fs.readFileSync ('./server/data/posts.json', 'utf8'));
+    let post = photoPosts.find((post) => String (req.query.id) === post.id);
+    post ? res.send (post) : res.status (404).end();
+});
+
+app.post('/getPhotoPosts', (req,res)=>{
+    let photoPosts = JSON.parse(fs.readFileSync('./server/data/posts.json', 'utf8'));
+    let filterConfig = req.body;
+    skip = req.query.skip || 0;
+    top = req.query.top || 10;
+
+    console.log ("parameters filterConfig: " + filterConfig.author);
+
+    photoPosts.forEach ((post) => {post.createdAt = new Date (post.createdAt);});
+
+    let result = [];
+    if (filterConfig !== undefined && "author" in filterConfig) {
+        photoPosts.forEach(function (value) {
+            if (value.author === filterConfig.author) {
+                result.push(value);
+            }
+        });
+    }
+    else if (filterConfig !== undefined && "createdAt" in filterConfig){
+        photoPosts.forEach(function (value) {
+            if (value.createdAt > filterConfig.createdAt) {
+                result.push(value);
+            }
+        });
+    }
+    else {
+        result = photoPosts.slice();
+    }
+
+    result.sort(function (a, b) {
+        return b.createdAt - a.createdAt;
+    });
+    result = result.slice(skip, top);
+
+    result ? res.send(result) : res.status(404).end();
+});
+
+app.post('/addPhotoPost', (req, res)=>{
+    let photoPosts = JSON.parse(fs.readFileSync('./server/data/posts.json', 'utf8'));
+    let newPost = req.body;
+    newPost.createdAt = new Date (newPost.createdAt);
+
+    let flag = true;
+    photoPosts.forEach ((post) => {if (post.id === newPost.id){
+    	flag = false;
+    }});
+
+    if (validatePhotoPost(newPost) && flag) {
+        photoPosts.push(newPost);
+        fs.writeFileSync('./server/data/posts.json', JSON.stringify(photoPosts));
+        res.send ("Post was added.");
+    }
+    else{
+    	res.send ("Post wasn't added.");
+    }
+    res.status(200).end();
+});
+
+let validatePhotoPost = function (post) {
+        if (post === undefined) {
+            return false;
+        }
+        if (!("id" in post && (typeof post.id === "string"))) {
+            return false;
+        }
+        if (!("description" in post && (typeof post.description === "string") && post.description.length < 200)) {
+            return false;
+        }
+        if (!("createdAt" in post && post.createdAt instanceof Date)) {
+            return false;
+        }
+        if (!("author" in post && (typeof post.author === "string"))) {
+            return false;
+        }
+        if (!("photoLink" in post && (typeof post.author === "string"))) {
+            return false;
+        }
+
+        return true;
+    };
+
+app.put('/editPhotoPost', (req,res)=>{
+    let photoPosts = JSON.parse(fs.readFileSync('./server/data/posts.json', 'utf8'));
+    let post = req.body;
+    if (!post) {
+        res.status(404).end();
+    }
+
+    let result = photoPosts.find(function (item) {
+        return item.id === String (req.query.id);
+    });
+
+    if (result === null) {
+        res.send ("Don't find post with id = " + post.id);
+    }
+    else {
+        if ("description" in post && (typeof post.description === "string") && post.description.length < 200) {
+            result.description = post.description;
+        }
+        if ("photoLink" in post && (typeof post.photoLink === "string")) {
+            result.photoLink = post.photoLink;
+        }
+        res.send ("Post successfully changed.");
+    }
+    res.status(200).end();
+});
+
+app.delete('/removePhotoPost', (req, res)=>{
+    let photoPosts = JSON.parse(fs.readFileSync('./server/data/posts.json', 'utf8'));
+    let result = photoPosts.find(function (item) {
+        return item.id === String (req.query.id);
+    });
+
+    if (result === null) {
+    	res.status(404).end();
+        res.send ("Don't find post");
+    }
+    else {
+        photoPosts.splice(photoPosts.indexOf(result), 1);
+        res.send (result);
+        res.status(200).end(); 
+    }
+});
