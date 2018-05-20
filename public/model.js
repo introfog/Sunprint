1a7ed9210e9d5eaf33af1790cb82b4526a4d8a55
introfog <chubrickdm@gmail.com>
2018-05-20 12:40:01 +0300

    Clean code with eslint

---
+++ public/model.js
@@ -0,0 +1,320 @@
+var initialPostsDate = [
+    {
+         id: "1",
+         description: 'Женская сборная Беларуси выиграла эстафету.',
+         createdAt: new Date('2018-02-23T23:00:00'),
+         author: 'Денис',
+         photoLink: 'Photos\\Photo1.png'
+     },
+     {
+         id: "2",
+         description: 'Сегодня очень снежно.',
+         createdAt: new Date('2018-03-23T23:00:00'),
+         author: 'Сергей',
+         photoLink: 'Photos\\Photo2.png'
+     },
+     {
+         id: "3",
+         description: 'На улице жара неимоверная.',
+         createdAt: new Date('2017-10-23T23:00:15'),
+         author: 'Максим',
+         photoLink: 'Photos\\Photo3.png'
+     },
+     {
+         id: "4",
+         description: 'Поеду в Питер завтра.',
+         createdAt: new Date('2016-02-23T23:00:00'),
+         author: 'Катя',
+         photoLink: 'Photos\\Photo4.png'
+     },
+     {
+         id: "5",
+         description: 'Завтра собираюсь в путешествие, кто со мной?',
+         createdAt: new Date('2018-05-23T23:00:00'),
+         author: 'Ольга',
+         photoLink: 'Photos\\Photo5.png'
+     },
+     {
+         id: "6",
+         description: 'Люблю гулять под дождем, очень бодрит кстати.',
+         createdAt: new Date('2018-12-23T23:00:00'),
+         author: 'Сергей3001',
+         photoLink: 'Photos\\Photo6.png'
+     },
+     {
+         id: "7",
+         description: 'Не знаю что написать, идеи закончились.',
+         createdAt: new Date('2018-02-13T23:00:00'),
+         author: 'Киллер2007',
+         photoLink: 'Photos\\Photo7.png'
+     },
+     {
+         id: "8",
+         description: 'Какое-то описание.',
+         createdAt: new Date('2018-02-23T15:00:00'),
+         author: 'Димончик',
+         photoLink: 'Photos\\Photo8.png'
+     },
+     {
+         id: "9",
+         description: 'Это только 9-ое описание, а идей нет.',
+         createdAt: new Date('2018-09-23T23:00:00'),
+         author: 'Димка',
+         photoLink: 'Photos\\Photo9.png'
+     },
+     {
+         id: "10",
+         description: 'Я родился.',
+         createdAt: new Date('1999-04-27T10:30:01'),
+         author: 'Дмитрий Михайлович',
+         photoLink: 'Photos\\Photo10.png'
+     },
+     {
+         id: "11",
+         description: 'Я побывал в 3 странах за месяц летом.',
+         createdAt: new Date('2010-10-23T23:00:00'),
+         author: 'Митя',
+         photoLink: 'Photos\\Photo11.png'
+     },
+     {
+         id: "12",
+         description: 'Вчера вечером я был в городе.',
+         createdAt: new Date('2016-02-23T13:34:00'),
+         author: 'Димка',
+         photoLink: 'Photos\\Photo12.png'
+     },
+     {
+         id: "13",
+         description: 'Ребята, намечается тусовка 15 марта, кто пойдет?',
+         createdAt: new Date('2018-03-25T13:00:00'),
+         author: 'Димка',
+         photoLink: 'Photos\\Photo13.png'
+     },
+     {
+         id: "14",
+         description: 'Вчера в Минске было очень много ДТП и снега было по колено.',
+         createdAt: new Date('2017-01-23T23:00:00'),
+         author: 'Дима',
+         photoLink: 'Photos\\Photo14.png'
+     },
+     {
+         id: "15",
+         description: 'А какие книги читали вы? Какие любимые жанры?',
+         createdAt: new Date('2018-04-23T13:00:00'),
+         author: 'Платон',
+         photoLink: 'Photos\\Photo15.png'
+     },
+     {
+         id: "16",
+         description: 'Можно ли сделать пустое описание?',
+         createdAt: new Date('2018-03-12T23:00:00'),
+         author: 'Аристарх',
+         photoLink: 'Photos\\Photo16.png'
+     },
+     {
+         id: "17",
+         description: 'Моя фантазия на сиходе, а брать чьи-то реальные описания долго.',
+         createdAt: new Date('2013-02-01T23:00:00'),
+         author: 'Михаил',
+         photoLink: 'Photos\\Photo17.png'
+     },
+     {
+         id: "18",
+         description: 'У меня еще куча дел, а времени не так уж и много.',
+         createdAt: new Date('2011-02-23T12:43:00'),
+         author: 'Евпатий',
+         photoLink: 'Photos\\Photo18.png'
+     },
+     {
+         id: "19",
+         description: 'Завтра в универ, но греет душу что в четверг выходной.',
+         createdAt: new Date('2018-03-24T16:00:00'),
+         author: 'Марк',
+         photoLink: 'Photos\\Photo19.png'
+     },
+     {
+         id: "20",
+         description: 'Фух, вот и закончил с описаниями.',
+         createdAt: new Date('1998-01-26T23:00:00'),
+         author: 'Игнатий',
+         photoLink: 'Photos\\Photo20.png'
+     }
+];
+
+
+let server = (function () {
+    let readPostsJSON = function (){
+        let result = [];
+        let reqServer = new XMLHttpRequest();
+        reqServer.open('GET', '/read', false);
+        reqServer.onreadystatechange = () => {
+            result = JSON.parse(reqServer.responseText);
+        };
+        reqServer.send();
+
+        return result;
+    };
+
+    let initialPostsJSON = function (){
+        let reqServer = new XMLHttpRequest();
+        reqServer.open('POST', '/write', false);
+        reqServer.setRequestHeader('Content-Type', 'application/json');
+
+        reqServer.send(JSON.stringify(initialPostsDate));
+    };
+
+    let writePostsJSON = function (posts){
+        let reqServer = new XMLHttpRequest();
+        reqServer.open('POST', '/write', false);
+        reqServer.setRequestHeader('Content-Type', 'application/json');
+
+        reqServer.send(JSON.stringify(posts));
+    };
+
+    
+    let getMaxPostID = function (){
+        let posts = readPostsJSON ();
+        let maxID = -1;
+        posts.forEach ((post) => {
+            if (Number (post.id) > maxID){
+                maxID = Number (post.id);
+            }
+        });
+        return maxID;
+    }
+
+    let getPhotoPost = function (id){
+        let data = '?id=' + encodeURIComponent(id);
+
+        let post;
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', '/getPhotoPost/' + data, false);
+        xhr.onreadystatechange = () => {
+            if (xhr.status === 200) {
+                post = xhr.responseText;
+            }
+        };
+        xhr.send();
+
+        post = JSON.parse (post);
+        post.createdAt = new Date (post.createdAt);
+        return post;
+    }
+
+    let getPhotoPostsPromise = function (skip, top, filterConfig){
+       return new Promise((resolve, reject) => {
+            let data = '?skip=' + encodeURIComponent(visiblePosts + skip) + '&top=' + encodeURIComponent(visiblePosts + top);
+            let body = JSON.stringify (filterConfig);
+
+            let posts;
+            let xhr = new XMLHttpRequest();
+            xhr.open('POST', '/getPhotoPosts/' + data, false);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.onreadystatechange = () => {
+                if (xhr.status === 200) {
+                    resolve (xhr.responseText);
+                }
+                else{
+                    reject();
+                }
+            };
+            xhr.send(body);
+        });
+    }
+
+    let getPhotoPosts = function (skip, top, filterConfig){
+        skip = skip || 0;
+        top = top || 10;
+
+        
+        let tempFunc = async (skip, top, filterConfig) => {
+            let posts;
+            await getPhotoPostsPromise(skip, top, filterConfig).then(
+                result => {
+                    posts = result;
+                    posts = JSON.parse (posts);
+                    posts.forEach((post)=>{
+                        post.createdAt = new Date (post.createdAt);
+                    });
+                    DOMmodule.showPhotoPostsFromServer (posts);
+                    
+                }, error => console.log ("Error"));
+        };
+
+        tempFunc(skip, top, filterConfig);
+    }
+
+    let addPhotoPostPromise = (post) => {
+        return new Promise((resolve, reject) => {
+            let xhr = new XMLHttpRequest();
+            xhr.open('POST', '/addPhotoPost/', true);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.onreadystatechange = () => {
+                if (xhr.status === 200) {
+                    resolve();
+                } 
+                else {
+                    reject();
+                }
+            };
+            xhr.send(JSON.stringify (post));
+        });
+    };
+
+    let addPhotoPost = function (post){
+        let flag = true;
+        addPhotoPostPromise(post).then(response => flag = true, error => flag = false);
+        return flag;
+    }
+
+    let removePhotoPostPromise = function (id) {
+        return new Promise((resolve, reject) => {
+            let xhr = new XMLHttpRequest();
+            xhr.open('DELETE', '/removePhotoPost/?id=' + id, true);
+            xhr.onreadystatechange = () => {
+                if (xhr.status === 200) {
+                    resolve();
+                }
+                else{
+                    reject();
+                }
+            };
+            xhr.send();
+        });
+    }
+
+    let removePhotoPost = function (id) {
+        let flag = true;
+        removePhotoPostPromise (id).then (response => flag = true, error => flag = false);
+        return flag;
+    }
+
+     let editPhotoPostPromise = function (id, photoPost) {
+        return new Promise((resolve, reject) => {
+            let data = '?id=' + encodeURIComponent(id);
+            let body = JSON.stringify (photoPost);
+
+            let xhr = new XMLHttpRequest();
+            xhr.open('PUT', '/editPhotoPost/' + data, false);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.onreadystatechange = () => {
+                if (xhr.status === 200) {
+                    resolve();
+                }
+                else{
+                    reject();
+                }
+            };
+            xhr.send(body);
+        });
+    };
+
+    let editPhotoPost = function (id, photoPost) {
+        let flag = true;
+        editPhotoPostPromise (id, photoPost).then (response => flag = true, error => flag = false);
+        return flag;
+    }
+
+    return {readPostsJSON, initialPostsJSON, writePostsJSON, getMaxPostID, getPhotoPost, 
+        getPhotoPosts, addPhotoPost, removePhotoPost, editPhotoPost};
+})();
--- public/scripts/control.js
+++ public/scripts/control.js
@@ -1,149 +1,156 @@
-let EventsHandlers = (function (){
-    let isEditPost = false;
+const EventsHandlers = (function () {
+  let isEditPost = false;
 
+  const handleRemovePhotoPost = function () {
+    DOMmodule.removePhotoPost(this.getAttribute('id'));
+  };
 
-    let handleRemovePhotoPost = function () {
-        DOMmodule.removePhotoPost(this.getAttribute("id"));
-    };
+  const handleSearchAuthor = function () {
+    DOMmodule.showPhotoPosts(true, 0, 10, { author: this.value });
+  };
 
-    let handleSearchAuthor = function () {
-        DOMmodule.showPhotoPosts(true, 0, 10, {author: this.value});
-    };
+  const handleSearchDate = function () {
+    DOMmodule.showPhotoPosts(true, 0, 10, { createdAt: new Date(this.value) });
+  };
 
-    let handleSearchDate = function () {
-        DOMmodule.showPhotoPosts(true, 0, 10, {createdAt: new Date (this.value)});
-    };
+  const handleResetSettings = function () {
+    DOMmodule.showPhotoPosts(true);
+    const searchAuthor = document.getElementById('search_author');
+    searchAuthor.value = '';
+    const searchDate = document.getElementById('search_date');
+    searchDate.value = '';
+  };
 
-    let handleResetSettings = function () {
-        DOMmodule.showPhotoPosts(true);
-        let searchAuthor = document.getElementById("search_author");
-        searchAuthor.value="";
-        let searchDate = document.getElementById("search_date");
-        searchDate.value="";
-    };
+  const handleShowMorePosts = function () {
+    DOMmodule.showPhotoPosts(false, 0, 10);
+  };
 
-    let handleShowMorePosts = function () {
-        DOMmodule.showPhotoPosts(false, 0, 10);
-    };
+  const handleLogIn = function () {
+    if (user === null) {
+      const modalWindow = document.getElementById('modal_window');
+      modalWindow.style.display = 'block';
+      const content = document.getElementById('log_in_modal_content');
+      content.style.display = 'block';
+    } else {
+      user = null;
+      localStorage.setItem('userName', user);
+      const logInBut = document.getElementById('log_in_button');
+      logInBut.innerText = 'Log in';
+      DOMmodule.checkAuthorization();
+      DOMmodule.showPhotoPosts();
+    }
+  };
 
-    let handleLogIn = function () {
-        if (user === null){
-            let modalWindow = document.getElementById("modal_window");
-            modalWindow.style.display = "block";
-            let content = document.getElementById("log_in_modal_content");
-            content.style.display = "block";
+  const handleCloseModalWin = function () {
+    const modalWindow = document.getElementById('modal_window');
+    modalWindow.style.display = 'none';
+    const logInContent = document.getElementById('log_in_modal_content');
+    logInContent.style.display = 'none';
+    const addPhotoContent = document.getElementById('add_edit_modal_content');
+    addPhotoContent.style.display = 'none';
+    document.getElementById('input_description').value = '';
+    document.getElementById('drag_drop_photo').textContent = 'Drop photo here';
+  };
+
+  const handleConfirmLogIn = function () {
+    const nickname = document.getElementById('log_in_nickname');
+    const password = document.getElementById('log_in_password');
+    if (nickname.value === 'null') {
+      alert('Change other name');
+      return;
+    }
+    if (nickname.value !== '') {
+      user = nickname.value;
+      localStorage.setItem('userName', user);
+      const logInBut = document.getElementById('log_in_button');
+      logInBut.innerText = user;
+      DOMmodule.checkAuthorization();
+      DOMmodule.showPhotoPosts();
+      handleCloseModalWin();
+      nickname.value = '';
+      password.value = '';
+    }
+  };
+
+  const handleAddPhotoPost = function () {
+    const modalWindow = document.getElementById('modal_window');
+    modalWindow.style.display = 'block';
+    const content = document.getElementById('add_edit_modal_content');
+    content.style.display = 'block';
+    isEditPost = false;
+    newPost.photoLink = 'Photos\\';
+  };
+
+  const handleFileSelect = function (event) {
+    event.stopPropagation();
+    event.preventDefault();
+
+    const files = event.dataTransfer.files; // FileList object.
+    const file = files[0];
+    if (file.type.indexOf('image') !== -1) {
+      newPost.photoLink = `Photos\\${file.name}`;
+      const dragDropPhoto = document.getElementById('drag_drop_photo');
+      dragDropPhoto.textContent = file.name;
+    } else {
+      alert('You can drop only image.');
+    }
+  };
+
+  const handleDragOver = function (event) {
+    event.stopPropagation();
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'copy';
+  };
+
+  const handleConfirmAddEdit = function () {
+    if (newPost.photoLink === 'Photos\\') {
+      alert('Drop photo!');
+    } else {
+      if (!isEditPost) {
+        newPost.id = `${server.getMaxPostID() + 1}`;
+        newPost.author = user;
+        newPost.createdAt = new Date();
+        newPost.description = document.getElementById('input_description').value;
+
+        const clone = {}; // новый пустой объект
+        for (const key in newPost) {
+          clone[key] = newPost[key];
         }
-        else{
-            user = null;
-            localStorage.setItem("userName", user);
-            let logInBut = document.getElementById("log_in_button");
-            logInBut.innerText = "Log in";
-            DOMmodule.checkAuthorization();
-            DOMmodule.showPhotoPosts();
-        }
-    };
+        DOMmodule.addPhotoPost(clone);
+      } else {
+        newPost.description = document.getElementById('input_description').value;
+        DOMmodule.editPhotoPost(newPost.id, newPost);
+      }
+      handleCloseModalWin();
+    }
+  };
 
-    let handleCloseModalWin = function () {
-        let modalWindow = document.getElementById("modal_window");
-        modalWindow.style.display = "none";
-        let logInContent = document.getElementById("log_in_modal_content");
-        logInContent.style.display = "none";
-        let addPhotoContent = document.getElementById("add_edit_modal_content");
-        addPhotoContent.style.display = "none";
-        document.getElementById("input_description").value = "";
-        document.getElementById("drag_drop_photo").textContent = "Drop photo here";
-    };
+  const handleEditPhotoPost = function (id) {
+    const modalWindow = document.getElementById('modal_window');
+    modalWindow.style.display = 'block';
+    const content = document.getElementById('add_edit_modal_content');
+    content.style.display = 'block';
 
-    let handleConfirmLogIn = function () {
-        let nickname = document.getElementById("log_in_nickname");
-        let password = document.getElementById("log_in_password");
-        if (nickname.value === "null"){
-            alert("Change other name");
-            return;
-        }
-        if (nickname.value !== ""){
-            user = nickname.value;
-            localStorage.setItem("userName", user);
-            let logInBut = document.getElementById("log_in_button");
-            logInBut.innerText = user;
-            DOMmodule.checkAuthorization();
-            DOMmodule.showPhotoPosts();
-            handleCloseModalWin();
-            nickname.value = "";
-            password.value = "";
-        }
-    };
+    isEditPost = true;
 
-    let handleAddPhotoPost = function () {
-        let modalWindow = document.getElementById("modal_window");
-        modalWindow.style.display = "block";
-        let content = document.getElementById("add_edit_modal_content");
-        content.style.display = "block";
-        isEditPost = false;
-        newPost.photoLink = 'Photos\\';
-    };
+    newPost = server.getPhotoPost(id);
+    document.getElementById('input_description').value = newPost.description;
+    document.getElementById('drag_drop_photo').textContent = newPost.photoLink;
+  };
 
-    let handleFileSelect = function (event){
-        event.stopPropagation();
-        event.preventDefault();
-
-        let files = event.dataTransfer.files; // FileList object.
-        let file = files[0];
-        if (file.type.indexOf("image") !== -1) {
-            newPost.photoLink = 'Photos\\' + file.name;
-            let dragDropPhoto = document.getElementById("drag_drop_photo");
-            dragDropPhoto.textContent = file.name;
-        }
-        else{
-            alert("You can drop only image.");
-        }
-    };
-
-    let handleDragOver = function (event){
-        event.stopPropagation();
-        event.preventDefault();
-        event.dataTransfer.dropEffect = 'copy';
-    };
-
-    let handleConfirmAddEdit = function () {
-        if (newPost.photoLink === 'Photos\\'){
-            alert("Drop photo!");
-        }
-        else{
-            if (!isEditPost) {
-                newPost.id = "" + (server.getMaxPostID () + 1);
-                newPost.author = user;
-                newPost.createdAt = new Date();
-                newPost.description = document.getElementById("input_description").value;
-
-                let clone = {}; // новый пустой объект
-                for (let key in newPost) {
-                    clone[key] = newPost[key];
-                }
-                DOMmodule.addPhotoPost(clone);
-            }
-            else{
-                newPost.description = document.getElementById("input_description").value;
-                DOMmodule.editPhotoPost(newPost.id, newPost);
-            }
-            handleCloseModalWin();
-        }
-    };
-
-    let handleEditPhotoPost = function (id) {
-        let modalWindow = document.getElementById("modal_window");
-        modalWindow.style.display = "block";
-        let content = document.getElementById("add_edit_modal_content");
-        content.style.display = "block";
-
-        isEditPost = true;
-
-        newPost = server.getPhotoPost(id);
-        document.getElementById("input_description").value = newPost.description;
-        document.getElementById("drag_drop_photo").textContent = newPost.photoLink;
-    };
-
-    return {handleRemovePhotoPost, handleSearchAuthor, handleResetSettings, handleSearchDate, handleShowMorePosts,
-    handleLogIn, handleCloseModalWin, handleConfirmLogIn, handleAddPhotoPost, handleFileSelect, handleDragOver,
-    handleConfirmAddEdit, handleEditPhotoPost};
-})();
+  return {
+    handleRemovePhotoPost,
+    handleSearchAuthor,
+    handleResetSettings,
+    handleSearchDate,
+    handleShowMorePosts,
+    handleLogIn,
+    handleCloseModalWin,
+    handleConfirmLogIn,
+    handleAddPhotoPost,
+    handleFileSelect,
+    handleDragOver,
+    handleConfirmAddEdit,
+    handleEditPhotoPost,
+  };
+}());
